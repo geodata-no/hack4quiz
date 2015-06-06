262c6658a2a3c2c6ff8f0f7b9903a721886c5d82
Kristian S. Førde <kfoerde@gmail.com>
2015-06-06 11:05:43 +0200

    Fixed small glitch in score

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -127,7 +127,7 @@ var kommuneAnswers = $.getJSON('geo/kommuner.geojson');
 			setTimeout(function(){
 				$('#riktig').hide();
 			},1000)
-			OnQuestionDone(Math.round(progressbar.value()*-1000));
+			OnQuestionDone((1000 - progressbar.value() * -999).toFixed(0));
 		}
 		console.log('Gjettet: '+ guess);
 
